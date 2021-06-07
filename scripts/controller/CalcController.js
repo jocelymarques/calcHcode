436caff4dd0dc329cb7466cd8337e4d4a521a402
Jocely Marques <37278449+jocelymarques@users.noreply.github.com>
2021-06-07 20:34:35 -0300

    first commit

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,66 @@
+class CalcController {
+    constructor() {
+        this._locale = "pt-BR"
+        this._displayCalcE1 = document.querySelector("#display");
+        this._dateE1 = document.querySelector("#data");
+        this._timeE1 = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize() {
+
+        this.setDisplayDateTime();
+
+        setInterval(() => {
+
+            this.setDisplayDateTime();
+
+        }, 1000);
+
+
+    }
+
+    setDisplayDateTime() {
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+            day: "2-digit",
+            month: "long",
+            year:"numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+    }
+
+    get displayTime() {
+        return this._timeE1.innerHTML;
+    }
+
+    set displayTime(value) {
+        return this._timeE1.innerHTML = value;
+    }
+
+    get displayDate() {
+        return this._dateE1.innerHTML;
+    }
+
+    set displayDate(value) {
+        return this._dateE1.innerHTML = value;
+    }
+
+    get displayCalc() {
+        return this._displayCalcE1.innerHTML;
+    }
+
+    set displayCalc(value) {
+        return this._displayCalcE1.innerHTML = value;
+    }
+
+    get currentDate() {
+        return new Date();
+    }
+
+    set currentDate(value) {
+        return this._currentDate = value;
+    }
+}
